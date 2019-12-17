a6a8c606dd72def140d8c1a777a0217300895f94
Alex <2426671397@qq.com>
2019-12-17 11:17:07 +0800

    2019-12-16

---
+++ environment/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/media/alex/新加卷/PythonProject/PythonFullStack/environment/lib"
+XML2_LIBS="-lxml2 -L/media/alex/新加卷/PythonProject/PythonFullStack/environment/lib -lz -L/media/alex/新加卷/PythonProject/PythonFullStack/environment/lib -llzma -lpthread -L/media/alex/新加卷/PythonProject/PythonFullStack/environment/lib  -lm "
+XML2_INCLUDEDIR="-I/media/alex/新加卷/PythonProject/PythonFullStack/environment/include/libxml2 -I/media/alex/新加卷/PythonProject/PythonFullStack/environment/include"
+MODULE_VERSION="xml2-2.9.9"
+
