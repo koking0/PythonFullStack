34c66e529f5147edd14bd4c20e4342a9242ce3b8
Alex <2426671397@qq.com>
2019-12-30 20:14:36 +0800

    2019-12-30

--- 第二模块 函数编程/2019-12-30/encode.py
+++ 第二模块 函数编程/2019-12-30/encode.py
@@ -7,10 +7,16 @@
 # @Software : PyCharm
 
 string = "Alex  天津科技大学"
-print(string.encode("utf-8"))
 
+print(string.encode("utf-8"))
 '''
 b'Alex  \xe5\xa4\xa9\xe6\xb4\xa5\xe7\xa7\x91\xe6\x8a\x80\xe5\xa4\xa7\xe5\xad\xa6'
 
-UTF-8 一个中为占三个字节
+UTF-8编码 一个中文占三个字节
 '''
+
+print(string.encode("gbk"))
+'''
+b'Alex  \xcc\xec\xbd\xf2\xbf\xc6\xbc\xbc\xb4\xf3\xd1\xa7'
+
+GBK编码 一个中文占两个字节'''
